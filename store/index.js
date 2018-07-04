e2118f50b250369a80ead9fbdb4b76f939572b35
Fredrik Gullberg <--global>
2018-07-04 22:05:33 +0200

    Added definition check for req.headers

--- store/index.js
+++ store/index.js
@@ -3,7 +3,7 @@ import cookieparser from "cookieparser";
 export const actions = {
   nuxtServerInit({ commit }, { req }) {
     let user = null;
-    if (req.headers.cookie) {
+    if (req && req.headers && req.headers.cookie) {
       const parsed = cookieparser.parse(req.headers.cookie);
       user = (parsed.user && JSON.parse(parsed.user)) || null;
     }
